c26377b034f2babc5fbc350e8991e9b5dac9e5a0
Ryan Miller <ryan@rmiller.dev>
2024-10-29 20:59:59 -0400

    add dependancy

--- src/main/java/dev/rmiller/thwompchatbackend/websocket/WebSocketConfig.java
+++ src/main/java/dev/rmiller/thwompchatbackend/websocket/WebSocketConfig.java
@@ -1,4 +1,13 @@
 package dev.rmiller.thwompchatbackend.websocket;
 
-public class WebSocketCongig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+
 }
